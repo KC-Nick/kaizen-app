aea5121243a034f98637500c96d7ee3eade8f201
KC-Nick <wilson.kasinichole@gmail.com>
2023-11-30 19:24:54 -0500

    fixed up handlebars

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,54 +1,54 @@
 const router = require('express').Router();
 const { Post, Comment } = require('../../models');
 
-router.get('/', async (req, res) => {
-  try {
-    // Get all posts and JOIN with user data
-    const postData = await Post.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-        Comment
-      ],
-    });
+// router.get('/', async (req, res) => {
+//   try {
+//     // Get all posts and JOIN with user data
+//     const postData = await Post.findAll({
+//       include: [
+//         {
+//           model: User,
+//           attributes: ['name'],
+//         },
+//         Comment
+//       ],
+//     });
 
-    // Serialize data so the template can read it
-    const posts = postData.map((post) => post.get({ plain: true }));
+//     // Serialize data so the template can read it
+//     const posts = postData.map((post) => post.get({ plain: true }));
 
-    // Pass serialized data and session flag into template
-    res.render('post', { 
-      posts, 
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     // Pass serialized data and session flag into template
+//     res.render('post', { 
+//       posts, 
+//       logged_in: req.session.logged_in 
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
-router.get('/post/:id', async (req, res) => {
-  try {
-    const postData = await {Post}.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-        Comment
-      ],
-    });
+// router.get('/post/:id', async (req, res) => {
+//   try {
+//     const postData = await {Post}.findByPk(req.params.id, {
+//       include: [
+//         {
+//           model: User,
+//           attributes: ['name'],
+//         },
+//         Comment
+//       ],
+//     });
 
-    const post = postData.get({ plain: true });
+//     const post = postData.get({ plain: true });
 
-    res.render('post', {
-      ...post,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.render('post', {
+//       ...post,
+//       logged_in: req.session.logged_in
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 router.post('/', async (req, res) => {
   try {
