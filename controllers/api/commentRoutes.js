afeebcbd30bd2f23db49acc01e17695e9ba48a10
KC-Nick <139719585+KC-Nick@users.noreply.github.com>
2023-12-04 20:25:56 -0500

    fixed sign up code to make profiles (#17)
    * adding boilerplate code and files in mvc structure
    
    * fixing name
    
    * added base files, folders, and some boilerplate
    
    * added gitignore file
    
    * added code for all router files
    
    * working on models for user, comment, and post
    
    * fixing up models and routes code for post model
    
    * added seeds stuff
    
    * fixed up handlebars
    
    * worked on seeds
    
    * fixed posts/:id route to get individual posts
    
    * added code for creating comments
    
    * fixed sign up code
    
    ---------
    
    Co-authored-by: jaxe93 <jaxe93work@gmail.com>

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,34 +1,35 @@
 const router = require('express').Router();
 const { Comment, User, Post } = require('../../models');
 
-router.get('/:id', async (req, res) => {
-    //get comment through id, including the post associated with it
-    try {
-        const commentData = await Comment.findByPk(req.params.id, {
-            include: [{
-                model: User,
-                attributes: ['name']
-            },
-            {
-                model: Post,
-                atrributes: ['id']
-            }
-        ]});
+// router.get('/:id', async (req, res) => {
+//     //get comment through id, including the post associated with it
+//     try {
+//         const commentData = await Comment.findByPk(req.params.id, {
+//             include: [{
+//                 model: User,
+//                 attributes: ['name']
+//             },
+//             {
+//                 model: Post,
+//                 atrributes: ['id']
+//             }
+//         ]});
 
-        if (!commentData) {
-            res.status(404).json({ message: 'No comment found with this id!' });
-            return;
-        }
+//         if (!commentData) {
+//             res.status(404).json({ message: 'No comment found with this id!' });
+//             return;
+//         }
 
-        res.status(200).json(commentData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+//         res.status(200).json(commentData);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 router.post('/', async (req, res) => {
     // create a new comment
     try {
+        console.log(req.session);
         const commentData = await Comment.create(
             {
                 description: req.body.description,
