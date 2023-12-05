f1cd449459b27f603a2ab79d975f513edc99b9fa
KC-Nick <wilson.kasinichole@gmail.com>
2023-12-04 22:08:28 -0500

    fixed posting and deleting issue

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,11 +1,12 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#name').value.trim();
-  const description = document.querySelector('#desc').value.trim();
+  const name = document.querySelector('#goal-name').value.trim();
+  const description = document.querySelector('#goal-desc').value.trim();
   const timeframe = document.querySelector('#timeframe').value.trim();
 
   if (name && description && timeframe) {
+    console.log("posting")
     const response = await fetch(`/api/posts`, {
       method: 'POST',
       body: JSON.stringify({ name, description, timeframe }),
@@ -16,7 +17,6 @@ const newFormHandler = async (event) => {
 
     if (response.ok) {
       document.location.reload();
-      //window.location.reload();
     } else {
       alert('Failed to create post');
     }
